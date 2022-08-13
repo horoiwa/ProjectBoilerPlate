9cef80d38fed191a5e62729fa07dcaede01cecfd
horoiwa <horoiwa195@gmail.com>
2022-08-13 16:15:00 +0000

    dev

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,15 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../project/notebooks'))
 
 project = 'project'
 copyright = '2022, horoiwa'
 author = 'horoiwa'
-
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -34,7 +38,3 @@ import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
