47536734d02c8c68dd5c8906798f33ebb25a2aea
horoiwa <horoiwa195@gmail.com>
2022-08-13 16:22:04 +0000

    dev

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../project/notebooks'))
+sys.path.insert(0, os.path.abspath('../project/'))
 
 project = 'project'
 copyright = '2022, horoiwa'
