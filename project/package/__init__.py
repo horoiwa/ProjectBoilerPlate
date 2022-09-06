e7228ccfb956f3f28dad02d1245e4345d3b7f4b2
horoiwa <horoiwa195@gmail.com>
2022-09-06 12:41:25 +0000

    dev

---
+++ project/package/__init__.py
@@ -0,0 +1,1 @@
+from package.constants import config, logger
--- project/package/constants.py
+++ project/package/constants.py
@@ -3,14 +3,16 @@ pathなどの定数やaws/gcpクライアントのようなグローバルにひ
 """
 import datetime
 import json
+import copy
 import os
 from pathlib import Path
 from typing import Literal
+import sys
+import logging
 
 from pydantic.dataclasses import dataclass
 from pydantic.json import pydantic_encoder
 
-from package.common.setup_logger import setup_logger
 from package.module1.config import MD1Config
 
 HOME: Path = Path(__file__).resolve().parents[1]
@@ -18,6 +20,8 @@ ROOT: Path = Path(__file__).resolve().parents[0]
 CACHE_DIR: Path = ROOT / "__cache__"
 
 CONFIG_FILENAME = os.environ.get("CONFIG_FILENAME", "config.json")
+
+TODAY: str = datetime.datetime.today().strftime("%Y%m%d")
 
 
 @dataclass
@@ -52,6 +56,59 @@ def load_config():
 
 
 config = load_config()
+
+# From https://pod.hatenablog.com/entry/2020/03/01/221715
+cmap = {
+    "TRACE": "[TRACE]",
+    "DEBUG": "\x1b[0;36mDEBUG\x1b[0m",
+    "INFO": "\x1b[0;32mINFO\x1b[0m",
+    "WARNING": "\x1b[0;33mWARN\x1b[0m",
+    "WARN": "\x1b[0;33mwWARN\x1b[0m",
+    "ERROR": "\x1b[0;31mERROR\x1b[0m",
+    "ALERT": "\x1b[0;37;41mALERT\x1b[0m",
+    "CRITICAL": "\x1b[0;37;41mCRITICAL\x1b[0m",
+}
+
+
+class ColoredStreamHandler(logging.StreamHandler):
+    def emit(self, record: logging.LogRecord) -> None:
+        record = copy.deepcopy(record)
+        record.levelname = cmap[record.levelname]
+        super().emit(record)
+
+
+def setup_logger():
+
+    log_dir: Path = Path("/log") / TODAY
+    if not log_dir.exists():
+        log_dir.mkdir()
+
+    logger_name = "project"
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        "[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d %(message)s",
+        datefmt="%m/%d:%I:%M",
+    )
+
+    stream_handler = ColoredStreamHandler(stream=sys.stdout)
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler(log_dir / "log.txt", mode="a", encoding="utf-8")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    warning_handler = logging.FileHandler(log_dir / "warning.txt", mode="a", encoding="utf-8")
+    warning_handler.setLevel(logging.WARN)
+    warning_handler.setFormatter(formatter)
+    logger.addHandler(warning_handler)
+
+    return logger
+
 
 logger = setup_logger()
 
