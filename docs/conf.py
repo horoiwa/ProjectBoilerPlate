fc2ff0c322597d33955e141ba300d1a4e7658076
horoiwa <horoiwa195@gmail.com>
2022-09-19 12:51:51 +0000

    misc fix

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = 'en'
+language = 'ja'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
